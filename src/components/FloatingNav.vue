8adc7bee07e49344047963bfd3556196586bcf62
lucijaba7 <babic.lucija1204@gmail.com>
2021-05-18 21:42:45 +0200

    menu

---
+++ src/components/FloatingNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- <v-navigation-drawer floating app right permanent mini-variant>
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+  </v-navigation-drawer> -->
+  <!-- <v-app-bar dense dark floating collapse>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+  <span class="floating-nav float-right">
+    <v-btn icon>
+      <v-icon>mdi-cog</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-account-circle</v-icon>
+    </v-btn>
+  </span>
+  <!-- </v-app-bar> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { icon: "mdi-account-circle", text: "HOME", route: "/" },
+        { icon: "mdi-account-circle", text: "bla", route: "/" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.floating-nav {
+  margin: 30px;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-navigation-drawer class="secondary" app fixed permanent expand-on-hover>
+    <v-list nav>
+      <v-list-item
+        class="navigation-items"
+        v-for="link in links"
+        :key="link.text"
+        router
+        :to="link.route"
+      >
+        <v-list-item-icon>
+          <v-icon color="white">{{ link.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title class="white--text">{{
+          link.text
+        }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { icon: "mdi-home", text: "HOME", route: "/" },
+        {
+          icon: "mdi-silverware-fork-knife",
+          text: "RECIPES",
+          route: "/recipes",
+        },
+        { icon: "mdi-fridge", text: "FRIDGE", route: "/fridge" },
+        { icon: "mdi-cart", text: "GROCHERY LIST", route: "/grocheryList" },
+        { icon: "mdi-feature-search", text: "BROWSE", route: "/browse" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
