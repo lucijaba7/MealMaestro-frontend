4d3c0e63e84c883b34a5b9dd90a9938b80de2aa5
Dejana Pivac <dpivac@student.unipu.hr>
2021-05-18 22:11:03 +0200

    Registration

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,17 @@
 <template>
-   <v-app id="inspire">
-      <v-main>
+      
          <v-container fluid fill-height justify-center>
             <!-- <v-container align-center justify-center> -->
                <v-flex xs12 sm6 md4>
                   <v-card class="elevation-0 ">                     
                      <v-card-text class="py-2">
-                        <v-row justify="center" xs6>                   
-                    <v-col cols="12" md="8"  flat>
+                        <v-row justify="center" >                   
+                      <v-col cols="12" xs="4" md="8"  flat>
                        <v-card                   
                         class="mx-auto"                   
-                         
-                        flat                                       
+                        flat                                                             
                         >
-                      <v-img justify-center xs6                   
+                      <v-img justify-center                   
                         src="../assets/LOGO-tocni.png"
                         />
                       </v-card >                      
@@ -23,6 +21,7 @@
                           <h2 class="py-5 justify-center">Sign in</h2 >
                         </v-card-title>
                         <v-form>
+                          
                            <v-text-field class="py-3"                          
                               name="login"
                               label="Email"
@@ -34,28 +33,26 @@
                               label="Password"
                               type="password"
                            ></v-text-field>
+                            
                         </v-form>
-                        <v-card-actions class="justify-center">
-                        <v-spacer></v-spacer>
-                        <div class="text-center pb-8">
+                        <v-card-actions xs3 md4 class="justify-center">                        
+                        <div class="text-center pb-4">
                           <v-btn  rounded class="px-15 py-3 mt-5 green elevation-0" to="/">Login</v-btn>
                         </div>
                      </v-card-actions>
                      <v-layout row>
-                     <v-flex xs12 md6 justify-center>
-                       <p class="pt-5 px-2 justify-center" justify-center>New to MealMaestro?</p>
+                     <v-flex xs6 md6 justify-center class="text-right ">
+                       <p class="pt-5 ">New to MealMaestro?</p>
                      </v-flex>
-                     <v-flex xs12 md6>
-                       <v-btn depressed  class="pt-7 transparent "  to="/registration">Create account</v-btn>
+                     <v-flex xs6 md6 class="pt-5 transparent text-left ">
+                       <v-btn small depressed  class="transparent text-left "  to="/registration">Create account</v-btn>
                      </v-flex>
                      </v-layout>
-                     </v-card-text>
-                     
+                     </v-card-text>                     
                   </v-card>
                </v-flex>
             </v-container>
-      </v-main>
-   </v-app>
+     
 </template>
 
 <script>
---
+++ src/views/Registration.vue
@@ -0,0 +1,123 @@
+<template>
+         <v-container fluid fill-height justify-center>
+            <!-- <v-container align-center justify-center> -->
+               <v-flex xs12 sm6 md4>
+                  <v-card class="elevation-0 ">                     
+                     <v-card-text class="py-2"> 
+                         <v-row justify="center" >                          
+                        <v-card-title class="justify-center">
+                          <h2 class="justify-center">Registration</h2 >
+                        </v-card-title>    
+                         </v-row>  
+                    <v-row justify="center">         
+                    <v-col cols="12" xs="6" sm="6" md="6"  flat>
+                         <v-card                   
+                        class="mx-auto"                   
+                        flat                                                             
+                        >
+                    <v-card-actions xs3 md4 class="justify-center">                        
+                        <div class="text-center pb-4">
+                          <v-btn  rounded class="px-15 py-3 mt-5 " to="/">
+                            <v-img src="../assets/choose-avatar.png">
+                                
+                            </v-img>
+                          </v-btn>
+                        </div>
+                     </v-card-actions>
+                    <!-- <div class="text-center pb-4">
+                    <v-btn  absolut depressed color="px-15 transparent align-center ma-10">
+                      <v-img  class="mb-3"                
+                        src="../assets/choose-avatar.png"
+                        
+                        />
+                    </v-btn> 
+                    </div> -->
+                      </v-card >
+                       <!-- <v-flex class="mb-4"> -->
+                        <!-- <v-avatar size="96" class="mr-4">
+                            <img :src="'/avatars/avatar_' + (form.avatar.toLowerCase()) + '.png'" alt="Avatar">
+                        </v-avatar>
+                        <v-btn @click="openAvatarPicker">Change Avatar</v-btn> -->
+                    <!-- </v-flex>                      -->
+                    </v-col>
+                  </v-row>
+                         <v-form>
+                          <v-text-field                           
+                              name="login"
+                              label="Username"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field                          
+                              name="login"
+                              label="First name"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field                          
+                              name="login"
+                              label="Last name"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field                           
+                              name="login"
+                              label="Email"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field 
+                              id="password"                              
+                              name="password"
+                              label="Password"
+                              type="password"
+                           ></v-text-field>
+                            <v-text-field 
+                              id="repPassword"                              
+                              name="repeatPassword"
+                              label="Repeat password"
+                              type="password"
+                           ></v-text-field>
+                        </v-form>
+                        <v-card-actions xs3 md4 class="justify-center">                        
+                        <div class="text-center pb-4">
+                          <v-btn  rounded class="px-15  green elevation-0" to="/">Next</v-btn>
+                        </div>
+                     </v-card-actions>
+                     <v-layout row justify-center>
+                     <v-flex xs6 md6 justify-center class="text-right ">
+                       <p class="pt-5 ">Already have an account?</p>
+                     </v-flex>
+                     <v-flex xs4 md6 class="pt-5 white text-left ">
+                       <v-btn small depressed color="transparent" class="white text-left "  to="/login">Sign in</v-btn>
+                     </v-flex>
+                     <v-flex xs12 md12 justify-center class="text-center ">
+                       <p class="pt-5 text-center ">By continuing, you agree to MealMaestro <strong>Terms of Use</strong> and confirm that you have read its <strong>Privacy Policy</strong>.</p>
+                     </v-flex>
+                     </v-layout>
+                     </v-card-text>                     
+                  </v-card>
+               </v-flex>
+            </v-container>
+</template>
+
+<script>
+import AvatarPicker from '../components/AvatarPicker.vue'
+
+export default {
+   name: 'Login',
+//    components: { AvatarPicker },
+   props: {
+      source: String,
+   },
+//    methods: {
+//             openAvatarPicker () {
+//                 this.showAvatarPicker = true
+//             },
+//             selectAvatar (avatar) {
+//                 this.form.avatar = avatar
+//             }
+//         }
+};
+
+</script>
+
+<style>
+
+</style>
