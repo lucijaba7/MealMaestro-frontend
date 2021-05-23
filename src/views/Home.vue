074c9e21559e29844408e03f5462fed04c454c2f
lucijaba7 <babic.lucija1204@gmail.com>
2021-05-23 13:45:03 +0200

    start homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,69 @@
 <template>
-  <v-container fluid class="pt-8">
+  <v-container fluid class="pt-8 ">
     <v-row>
-      <v-col col="12" sm="2">
-        <v-img
-          src="@/assets/Avatar.png"
-          alt="Avatar"
-          lazy-src
-          contain
-          height="100"
-        />
+      <v-col cols="8" sm="2" align="start">
+        <v-img src="@/assets/Avatar.png" alt="Avatar" lazy-src contain />
       </v-col>
-      <v-col xs="12" sm="10">
-        <v-col>
-          <div>Ciao Lucija!</div>
-          <div>
-            People who love to eat are always the best people
-          </div></v-col
+      <v-col cols="12" sm="8">
+        <div class="text-h5 font-weight-bold">Ciao Lucija!</div>
+        <div>
+          People who love to eat are always the best people
+        </div>
+      </v-col>
+      <FloatingNav />
+    </v-row>
+    <v-divider class="my-3"></v-divider>
+    <v-row class="my-3">
+      <v-spacer></v-spacer>
+      <v-col cols="12" sm="8" align="center">
+        <v-btn fab x-small class="accent elevation-0"
+          ><v-icon x-small>mdi-chevron-left</v-icon></v-btn
         >
-      </v-col>
+        <span class="mx-2">April 12 - April 18</span>
+        <v-btn fab x-small class="accent elevation-0"
+          ><v-icon x-small>mdi-chevron-right</v-icon></v-btn
+        ></v-col
+      ><v-col cols="12" sm="2" align="end">BOTUN</v-col>
     </v-row>
-    <h1 class="main_divider my-5"></h1>
-    <v-divider class="my-8"></v-divider>
-    <v-row>
-      <v-btn class="small_next"></v-btn>
-      a
-      <v-btn></v-btn>
-    </v-row>
-    <v-row>
+
+    <v-row :dense="this.$vuetify.breakpoint.name == 'xs'">
       <v-col
         align-self="center"
         v-for="weekday in week_days"
         :key="weekday['day']"
+        class="py-1"
       >
-        <CalendarButton :info="weekday"
+        <CalendarButton :info="weekday" @click="console.log('1')"
       /></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="5" md="4" lg="2" align="start"
+        ><v-select :items="meals" label="Meal" v-model="defaultMeal"></v-select
+      ></v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="3">
+        <v-img src="@/assets/meal1.png" class="rounded-xl"> </v-img>
+      </v-col>
+      <v-col cols="12" sm="9">
+        <v-row class="justify-space-between">
+          <v-col
+            ><div class="font-weight-bold">
+              Tomato, Peach, and Basil Salad
+            </div></v-col
+          ><v-col align="end"
+            ><div>aaa</div>
+            <!-- <v-row>
+              <v-col>COOKED</v-col>
+              <v-col
+                ><v-simple-checkbox hint="cooked"></v-simple-checkbox
+              ></v-col>
+            </v-row> -->
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col></v-col>
     </v-row>
   </v-container>
 </template>
@@ -51,13 +81,15 @@ export default {
         {
           day: "Wednesday",
           date: 25,
-          active: false,
+          active: true,
         },
         { day: "Thursday", date: new Date(2021, 1, 4).getDay(), active: false },
         { day: "Friday", date: new Date(2021, 1, 5).getDay(), active: false },
         { day: "Saturday", date: new Date(2021, 1, 6).getDay(), active: false },
         { day: "Sunday", date: new Date(2021, 1, 7).getDay(), active: false },
       ],
+      defaultMeal: "Breakfast",
+      meals: ["Breakfast", "Lunch", "Dinner", "Snack", "Dessert"],
     };
   },
   mounted() {
