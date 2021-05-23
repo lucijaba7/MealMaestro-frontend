a0a478f096e833a4d8ea0d818ad299774c01b7be
Dejana Pivac <dpivac@student.unipu.hr>
2021-05-23 13:51:44 +0200

    Gotov login

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,7 @@
     <!-- <Sidebar /> -->
 
     <v-main>
-<<<<<<< HEAD
-      <FloatingNav />
-      
-=======
       <!-- <FloatingNav /> -->
->>>>>>> f69bf8685fc0468c064731bebdfad55241b061e9
       <router-view></router-view>
     </v-main>
   </v-app>
